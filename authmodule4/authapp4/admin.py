5ccc898693699928569525558f86626c94a68fa0
tanuja009 <tanujapatidar01@gmail.com>
2024-09-19 15:54:38 +0530

    m

--- authmodule4/authapp4/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-@admin.register(mymodel)
-class mymodeladmin(admin.ModelAdmin):
-  list_display=['id','name','age','email','contact']
-  
